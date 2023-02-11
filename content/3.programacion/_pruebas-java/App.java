f2541fbc3906356af79e6bb1a1c52a9d343c744a
dario ureña <darioaxel@gmail.com>
2023-02-11 17:22:26 +0100

    no actualice todo

--- content/3.programacion/_pruebas-java/App.java
+++ content/3.programacion/_pruebas-java/App.java
@@ -5,17 +5,14 @@ class App {
   public static void main(String[] args) {
 
     // Creamos un objeto de la case Scanner que leerá desde la entrada del sistema, usando la coma como separador
-    Scanner input = new Scanner(System.in).useDelimiter(",");
-
+    Scanner input = new Scanner(System.in);
     System.out.print("Escribe una línea de texto: ");
 
     // Recupera la línea completa como cadena
-    while (input.hasNext()) {
-        String myText = input.next();
-        System.out.println("Token: " + myText);
-        if (input.hasNext())  System.out.println("Say Yeah!");
-        else System.out.println("Say Nai!");
-    }
+    String myText = input.nextLine();
+    for(var t : myText.split(",")) {
+      System.out.println(t);
+    }    
   
     // Cerramos el objeto Scanner
     input.close();
